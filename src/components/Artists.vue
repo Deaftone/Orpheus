6f02eb7d08b75be8f3d91756e991c9de5eccb531
112RG <ryleegeorge1@gmail.com>
2021-04-15 21:49:04 +1000

    Adding more pages

---
+++ src/components/Artists.vue
@@ -0,0 +1,14 @@
+<template>  
+<a>Test</a>
+</template>
+
+<script>
+export default {
+  name: 'Artists'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
