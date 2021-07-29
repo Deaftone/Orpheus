31930740627d7a140cf794025eca59254310b771
112RG <ryleegeorge1@gmail.com>
2021-07-29 15:42:48 +1000

    Basic working of playbar and playback

--- src/components/player/index.vue
+++ src/components/player/index.vue
@@ -1,84 +1,169 @@
 <template>
   <main class="">
-    <!-- Previous Button -->
-    <button
-      class="text-gray-800 font-bold py-2 px-4 co"
-      @click="previosTrack"
-    >
-      <font-awesome-icon
-        icon="fast-backward"
-        color="gray"
+    <a>{{ currentTrack }}</a>
+    <div id="controls">
+      <div
+        id="volume"
+        class="w-20"
+      >
+        <Slider
+          v-model="volume"
+          :step="-1"
+          :max="1"
+          :tooltips="false"
+          @update="volumeChange"
+        />
+      </div>
+      <!-- Previous Button -->
+      <button
+        class="text-gray-800 font-bold py-2 px-4 co"
+        @click="previosTrack"
+      >
+        <font-awesome-icon
+          icon="fast-backward"
+          color="gray"
+        />
+      </button>
+      <!-- Play Button -->
+      <button
+        class="text-gray-800 font-bold py-2 px-4 co"
+        @click="playPause"
+      >
+        <font-awesome-icon
+          :icon="currentIcon"
+          color="gray"
+        />
+      </button>
+      <!-- Next Button -->
+      <button
+        class="text-gray-800 font-bold py-2 px-4 co"
+        @click="nextTrack"
+      >
+        <font-awesome-icon
+          icon="fast-forward"
+          color="gray"
+        />
+      </button>
+      <audio
+        id="appPlayer"
+        ref="appPlayer"
+        name="appPlayer"
+        style="hidden"
       />
-    </button>
-    <!-- Play Button -->
-    <button
-      class="text-gray-800 font-bold py-2 px-4 co"
-      @click="playPause"
-    >
-      <font-awesome-icon
-        :icon="currentIcon"
-        color="gray"
+    </div>
+
+    <a>{{ eTime }} / {{ duration }}</a>
+    <div id="playbar">
+      <Slider
+        v-model="percentPlayed"
+        :step="-1"
+        :tooltips="false"
+        @update="barChange"
       />
-    </button>
-    <!-- Next Button -->
-    <button
-      class="text-gray-800 font-bold py-2 px-4 co"
-      @click="nextTrack"
-    >
-      <font-awesome-icon
-        icon="fast-forward"
-        color="gray"
-      />
-    </button>
-    <audio
-      id="appPlayer"
-      ref="myId"
-      style="hidden"
-    />
+    </div>
   </main>
 </template>
 
 <script>
+import Slider from '@vueform/slider'
+import '../../freqtimeupdate'
 
 export default ({
     name: "Player",
+    components: {
+        Slider
+    },
       data() {
-    return {
-      player: this.$refs.myId,
-      playing: false,
-      paused: false,
-      currentIcon: 'play'
-    }
-  },
+        return {
+          appPlayer: undefined,
+          currentTrack: '',
+          isPlaying: false,
+          paused: false,
+          currentIcon: 'play',
+          percentPlayed: 0,
+          eTime: '00:00',
+          volume: 0.2,
+          duration: '00:00'
+        }
+    },
+    mounted() {
+        this.$nextTick(function() {
+            this.appPlayer = this.$refs.appPlayer
+            this.appPlayer.volume = this.volume
+                this.$watch("isPlaying",function() {
+                    if(this.isPlaying) {
+                      console.log("Audio playback started.");
+                      //prevent starting multiple listeners at the same time
+                      if(!this.listenerActive) {
+                        console.log('Add listener')
+                        this.listenerActive=true;
+                        //for a more consistent timeupdate, include freqtimeupdate.js and replace both instances of 'timeupdate' with 'freqtimeupdate'
+                        //frequent.add(this.appPlayer, this.playbackListener);
+                        this.appPlayer.addEventListener("freqtimeupdate",this.playbackListener);
+                      }
+                    }
+                });
+        })
+    },
     methods: {
+        convertTime(seconds){
+                const format = val => `0${Math.floor(val)}`.slice(-2);
+                var hours = seconds / 3600;
+                var minutes = (seconds % 3600) / 60;
+                return [minutes, seconds % 60].map(format).join(":");
+        },
+        barChange(e){
+            var time = e / 100 * this.appPlayer.duration
+            var diff = Math.abs( time - this.appPlayer.currentTime );
+            if(diff > 0.1) {
+                console.log(diff)
+                this.appPlayer.currentTime = time
+            }
+        },
         previosTrack(){
+            this.playTrack('brightside.flac')
             console.log('Previous track clicked')
         },
         nextTrack() {
+            this.playTrack('sober.flac')
             console.log('Next track clicked')
+        },
+        volumeChange (e) {
+            this.appPlayer.volume = e
         },
         playPause() {
             if(this.paused) {
                 console.log('Play/Pause -- Resume')
-                this.playing = true
+                this.isPlaying = true
                 this.paused = false
-                this.$refs.myId.play()
+                this.appPlayer.play()
                 this.currentIcon = "pause"
-            } else if (!this.$refs.myId.src) {
+            } else if (!this.appPlayer.src) {
                 console.log('Play/Pause -- Inital play load track')
-                this.$refs.myId.src = '/public/brightside.flac'
-                this.$refs.myId.play()
-                this.$refs.myId.volume = 0.4
-                this.playing = true
-                this.currentIcon = "pause"
+                this.playTrack('brightside.flac')
             } else {
                 console.log('Play/Pause -- Pause')
-                this.playing = false
+                this.isPlaying = false
                 this.paused = true
-                this.$refs.myId.pause()
+                this.appPlayer.pause()
                 this.currentIcon = "play"
             }
+        },
+        playTrack(track) {
+                this.appPlayer.src = track
+                this.appPlayer.play()
+                this.isPlaying = true
+                this.currentIcon = "pause"
+                this.currentTrack = this.appPlayer.src
+        },
+        playbackListener() {
+            var percentage = (this.appPlayer.currentTime / this.appPlayer.duration) * 100;
+            this.duration = this.convertTime(this.appPlayer.duration);
+            this.percentPlayed = percentage
+            var seconds = this.appPlayer.currentTime;
+            this.eTime = this.convertTime(seconds);
+            this.playbackTime = this.appPlayer.currentTime;
         }
-    }
+    },
 })
 </script>
