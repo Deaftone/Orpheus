43873ad4cc4e0f29d3de657f11c0a180399da941
112RG <ryleegeorge1@gmail.com>
2021-07-29 11:43:40 +1000

    Add font awesome

--- src/main.js
+++ src/main.js
@@ -2,4 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import './index.css'
-createApp(App).use(router).mount('#app')
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faPlay, faPause, faFastForward, faFastBackward } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+library.add(faPlay)
+library.add(faFastBackward)
+library.add(faFastForward)
+library.add(faPause)
+createApp(App).use(router).component("font-awesome-icon", FontAwesomeIcon).mount('#app')
