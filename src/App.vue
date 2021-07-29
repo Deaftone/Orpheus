60eea37d052babbcf11a0e97037c8c2e50222bee
112RG <ryleegeorge1@gmail.com>
2021-07-29 11:43:20 +1000

    Testing

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,132 @@
-<template class="flex">  
-   <div class="flex flex-col justify-between h-screen bg-gray-800" style="width:250px;position:fixed">
-      <header class="bg-gray-800 py-4 text-left text-2xl font-bold" style="z-index:2;padding-left:20px;">
-        <h1 class="bg-clip-text text-transparent bg-gradient-to-r from-pink-500 via-blue-300 to-pink-900" @click='goTo("")' >Orpheus</h1> 
+<template>  
+  <main class="h-screen flex">
+    <div class="w-72 h-screen bg-gray-500 overflow-hidden">
+      <header class="bg-gray-900 py-5 text-center text-2xl font-bold">
+        <h1
+          class="bg-clip-text text-transparent bg-gradient-to-r from-pink-500 via-blue-300 to-pink-900"
+          @click="goTo(&quot;&quot;)"
+        >
+          Orpheus
+        </h1> 
       </header>
-      <header class="bg-gray-900 py-4 w-screen" style="position:fixed;background-color:rgba(23, 23, 23)">
-        HEADER
-      </header>
-      <div class="text-left" style="padding-left:20px;padding-top:10px">
+      <nav class="bg-gray-900 h-full p-4">
         <a class="text-xl font-bold">My Library</a>
         <ul style="padding-left:20px;padding-top:5px">
-          <li class="s-item" @click='goTo("test")' >Albums</li>
-          <li class="s-item" @click='goTo("arists")'>Artists</li>
-          <li class="s-item">Tracks</li>
+          <li
+            class="s-item"
+            @click="goTo(&quot;test&quot;)"
+          >
+            Albums
+          </li>
+          <li
+            class="s-item"
+            @click="goTo(&quot;arists&quot;)"
+          >
+            Artists
+          </li>
+          <li class="s-item">
+            Tracks
+          </li>
+          <li class="s-item">
+            Tracks
+          </li>
+          <li class="s-item">
+            Tracks
+          </li>
+          <li class="s-item">
+            Tracks
+          </li>
+          <li class="s-item">
+            Tracks
+          </li>
+          <li class="s-item">
+            Tracks
+          </li>
+          <li class="s-item">
+            Tracks
+          </li>
+          <li class="s-item">
+            Tracks
+          </li>
+          <li class="s-item">
+            Tracks
+          </li>
+          <li class="s-item">
+            Tracks
+          </li>
+          <li class="s-item">
+            Tracks
+          </li>
+          <li class="s-item">
+            Tracks
+          </li>
+          <li class="s-item">
+            Tracks
+          </li>
+          <li class="s-item">
+            Tracks
+          </li>
+          <li class="s-item">
+            Tracks
+          </li>
+          <li class="s-item">
+            Tracks
+          </li>
+          <li class="s-item">
+            Tracks
+          </li>
+          <li class="s-item">
+            Tracks
+          </li>
+          <li class="s-item">
+            Tracks
+          </li>
+          <li class="s-item">
+            Tracks
+          </li>
+          <li class="s-item">
+            Tracks
+          </li>
+          <li class="s-item">
+            Tracks
+          </li>
+          <li class="s-item">
+            Tracks
+          </li>
+          <li class="s-item">
+            Tracks
+          </li>
+          <li class="s-item">
+            Tracks
+          </li>
+          <li class="s-item">
+            Tracks
+          </li>
         </ul>
-      </div>
-      <footer class="mt-auto bg-gray-800 py-4 w-screen">
-        FOOTER
-      </footer>
+      </nav>
     </div>
-    <div class="flex flex-col justify-between bg-gray-900" style="margin-left:250px;margin-top:50px;margin-bottom:50px;width:100%;">
-      <div id="content" style="padding:20px">
-        <router-view/>
-      </div>
+    <div class="flex flex-col justify-between overflow-hidden w-full bg-red-400">
+      <header class="bg-gray-900 py-5 text-center">
+        Content Header
+      </header>
+      <section class="overflow-y-scroll">      
+        <router-view />
+      </section>
     </div>
+    <footer class="mt-auto bg-gray-900 p-5 absolute bottom-0 w-full text-center">
+      Content Footer
+      <Player />
+    </footer>
+  </main>
 </template>
 
 <script>
-
+import Player from "./components/player/index.vue"
 export default {
   name: 'App',
+    components: {
+        Player
+    },
   methods: {
     goTo(p) {
       console.log(p)
@@ -39,22 +137,13 @@ export default {
 </script>
 
 <style>
-body {
-  background-color: rgba(23, 23, 23);
-}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  display: flex;
   color: white;
-  overflow:auto
 }
 
-.s-item {
-
-}
 .s-item:hover {
   color: rgba(244, 114, 182)
 }
