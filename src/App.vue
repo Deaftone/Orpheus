ce032a23e78d83bce3a17b507f7796d533d93d04
112RG <ryleegeorge1@gmail.com>
2021-07-30 13:34:27 +1000

    Implement searchbox

--- src/App.vue
+++ src/App.vue
@@ -105,15 +105,21 @@
         </ul>
       </nav>
     </div>
-    <div class="flex flex-col justify-between overflow-hidden w-full bg-red-400">
-      <header class="bg-gray-900 py-5 text-center">
+    <div
+      class="flex flex-col justify-between  overflow-hidden w-full bg-red-400"
+      style="padding-bottom:4.5rem"
+    >
+      <header class="bg-gray-900 p-2 text-center">
+        <div class="flex float-right">
+          <SearchBox />
+        </div>
         Content Header
       </header>
-      <section class="overflow-y-scroll">      
+      <section class="overflow-y-scroll ">      
         <router-view />
       </section>
     </div>
-    <footer class="mt-auto bg-gray-900 p-5 absolute bottom-0 w-full text-center">
+    <footer class="mt-auto bg-gray-900 p-2 absolute bottom-0 w-full text-center">
       <Player />
     </footer>
   </main>
@@ -121,10 +127,13 @@
 
 <script>
 import Player from "./components/player/index.vue"
+import SearchBox from "./components/SearchBox.vue"
+
 export default {
   name: 'App',
     components: {
-        Player
+        Player,
+        SearchBox
     },
   methods: {
     goTo(p) {
@@ -145,5 +154,17 @@ export default {
 
 .s-item:hover {
   color: rgba(244, 114, 182)
+}
+
+:root {
+  --slider-connect-bg: rgba(244, 114, 182);
+  --slider-handle-ring-color: #3B82F630;
+  --slider-handle-bg: rgb(65, 65, 65);
+  --slider-bg: rgb(65, 65, 65);
+  --slider-handle-width: 12px;
+  --slider-handle-height: 12px;
+  --slider-height: 4px;
+  --slider-handle-ring-width: 2px;
+  --slider-tooltip-radius: 2px;
 }
 </style>
