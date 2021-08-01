e5cc48622ba8f8c58975aaa8702b20c14f7f17b0
112RG <ryleegeorge1@gmail.com>
2021-08-01 23:26:45 +1000

    Add axios

---
+++ src/utils/apiAxios.js
@@ -0,0 +1,21 @@
+import axios from 'axios'
+const apiAxios = axios.create({
+  baseURL: 'http://192.168.1.13:4533/rest/',
+  timeout: 1000,
+  headers: { 'Content-Type': 'application/json' },
+  params: { 
+    u: '***REMOVED***',
+    t: '***REMOVED***',
+    s: '558dbf',
+    f: 'json',
+    c: 'Orpheus',
+    v: '1.8.0'
+  }
+})
+
+const getCover = function (id){
+  return `http://192.168.1.13:4533/rest/getCoverArt?u=${apiAxios.defaults.params.u}&s=${apiAxios.defaults.params.s}&t=${apiAxios.defaults.params.t}&f=json&c=Orpheus&v=1.8.0&id=${id}`
+}
+
+
+export default apiAxios
