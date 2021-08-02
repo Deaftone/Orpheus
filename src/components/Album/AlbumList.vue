f84505b242a58bdb1848ffe789ee840fb846b66a
112RG <ryleegeorge1@gmail.com>
2021-08-02 12:04:07 +1000

    Mock up album list page

---
+++ src/components/Album/AlbumList.vue
@@ -0,0 +1,48 @@
+<template>  
+  <div class="grid w-full grid-cols-1 gap-4 p-5 md:grid-cols-6 lg:grid-cols-10">
+    <div
+      v-for="album in albums"
+      :key="album.id"
+      class="flex justify-center p-1 transition-opacity rounded-lg bg-gradient-to-r from-yellow-400 via-red-500 to-pink-500"
+      @click=" $router.push({path: `/AlbumDetails/${album.id}`})
+      "
+    >
+      <div class="text-center transition-opacity">
+        <img
+          class="rounded-full"
+          :src="album.cover"
+        >
+        <a>{{ album.name }}</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from '../../utils/apiAxios'
+export default {
+  name: 'AlbumList',
+  data() {
+    return {
+      albums: [],
+    }
+  },
+  async mounted(){
+    const data = (await axios.get('/getAlbumList', {params: {
+      type: 'newest',
+      size: 500
+    }})).data
+    const albums = data['subsonic-response']['albumList']['album']
+    for(const album of albums){
+      console.log(album)
+      this.albums.push({name: album.title, id: album.id, cover: `http://192.168.1.13:4533/rest/getCoverArt?u=${axios.defaults.params.u}&s=${axios.defaults.params.s}&t=${axios.defaults.params.t}&f=json&c=Orpheus&v=1.8.0&id=${album.id}&size=300`})
+      
+    }
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
+
