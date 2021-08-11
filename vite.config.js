31a04914452f1d4aab262ce444c5ebd2d1c1c93f
112RG <ryleegeorge1@gmail.com>
2021-08-11 15:52:59 +1000

    Update vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,28 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: './',
-  plugins: [vue()]
+//import path from 'path'
+//import ViewLauncherVuePlugin from '@view-launcher/rollup-plugin-vue'
+import viteTips from 'vite-plugin-tips'
+export default defineConfig(({ mode }) => {
+  // load the .env file
+  // you may want to create one like `.env.development.local`
+  //const env = loadEnv(mode, __dirname)
+
+  return {
+    server: {
+      fs: {
+        // Allow serving files from one level up to the project root
+        allow: ['..']
+      }
+    },
+    plugins: [
+      /*       ViewLauncherVuePlugin({
+        entry: path.resolve(__dirname, 'src/main.js'),
+        theme: 'dark',
+        editor: env.VITE_EDITOR || 'vscode',
+      }), */
+      vue(),
+      viteTips(),
+    ],
+  }
 })
