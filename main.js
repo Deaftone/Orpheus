eadd5ddb1d35495f1d746139c136943698a7ef89
112RG <ryleegeorge1@gmail.com>
2021-08-23 19:45:55 +1000

    Disable electron frame. Set h and w

--- main.js
+++ main.js
@@ -7,8 +7,9 @@ const path = require('path')
 function createWindow () {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1920,
+    height: 1080,
+    frame: false,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
