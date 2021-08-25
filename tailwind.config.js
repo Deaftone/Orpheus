e5569df579a568575bb18d5c6b6d83b666acf3be
112RG <ryleegeorge1@gmail.com>
2021-08-25 23:29:35 +1000

    Add splash to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 const colors = require('tailwindcss/colors')
 module.exports = {
   purge: {
-    content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+    content: ['./index.html','splashscreen.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
     options: {
       safelist: [
         /data-theme$/,
