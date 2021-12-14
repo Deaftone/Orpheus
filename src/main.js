2bca79438d88507dbc4587bdad573e45f9f9c190
Rylee George <ryleegeorge1@gmail.com>
2021-12-15 00:16:45 +1100

    Add more fa icons

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import router from './router'
 import './index.css'
 import { library } from "@fortawesome/fontawesome-svg-core"
-import { faPlay, faPause, faFastForward, faFastBackward, faWindowClose, faWindowMaximize, faWindowMinimize } from "@fortawesome/free-solid-svg-icons"
+import { faPlay, faPause, faFastForward, faFastBackward, faWindowClose, faWindowMaximize, faWindowMinimize, faArrowLeft, faArrowRight } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome"
 import { store } from './store'
 library.add(faPlay)
@@ -11,6 +11,8 @@ library.add(faWindowMinimize)
 library.add(faWindowClose)
 library.add(faWindowMaximize)
 library.add(faFastBackward)
+library.add(faArrowLeft)
+library.add(faArrowRight)
 library.add(faFastForward)
 library.add(faPause)
 createApp(App).use(store).use(router).component("font-awesome-icon", FontAwesomeIcon).mount('#app')
