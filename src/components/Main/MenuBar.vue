9d27cdcee5ed1bd10d09ea1ecc9f8d92898ce778
Rylee George <ryleegeorge1@gmail.com>
2021-12-17 00:36:41 +1100

    Seperate out mainbar

---
+++ src/components/Main/MenuBar.vue
@@ -0,0 +1,48 @@
+<template> 
+  <div
+    id="menuBar"
+    class="h-6 navbar bg-base-100 text-neutral-content"
+  >
+    <div class="flex-1 hidden mx-2 lg:flex">
+      <span class="text-lg font-bold">
+        <button
+          class="px-4 py-2 font-bold text-gray-800 co"
+          @click="$router.go(-1)"
+        >
+          <font-awesome-icon
+            icon="arrow-left"
+            color="gray"
+          />
+        </button>
+        <button
+          class="px-4 py-2 font-bold text-gray-800 co"
+          @click="$router.go(1)"
+        >
+          <font-awesome-icon
+            icon="arrow-right"
+            color="gray"
+          />
+        </button>
+      </span>
+    </div> 
+    <div class="flex-1 lg:flex-none">
+      <SearchBox />
+    </div> 
+    <div class="flex-1 lg:flex-none">
+      <ThemeSwitcher />
+    </div>
+  </div>
+</template>
+
+<script>
+import ThemeSwitcher from "../Util/ThemeSwitcher.vue"
+import SearchBox from "../Util/SearchBox.vue"
+
+export default {
+  name: 'MenuBar',
+  components: {
+    SearchBox,
+    ThemeSwitcher
+  },
+}
+</script>
