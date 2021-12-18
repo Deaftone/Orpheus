93f9583a72f319d13500fe2a6438bcbb43e48122
Rylee George <ryleegeorge1@gmail.com>
2021-12-19 02:44:03 +1100

    Change store name

--- src/main.js
+++ src/main.js
@@ -5,7 +5,8 @@ import './index.css'
 import { library } from "@fortawesome/fontawesome-svg-core"
 import { faPlay, faPause, faFastForward, faFastBackward, faWindowClose, faWindowMaximize, faWindowMinimize, faArrowLeft, faArrowRight } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome"
-import { store } from './store'
+import { playerStore } from './stores/player'
+
 library.add(faPlay)
 library.add(faWindowMinimize)
 library.add(faWindowClose)
@@ -15,4 +16,5 @@ library.add(faArrowLeft)
 library.add(faArrowRight)
 library.add(faFastForward)
 library.add(faPause)
-createApp(App).use(store).use(router).component("font-awesome-icon", FontAwesomeIcon).mount('#app')
+
+createApp(App).use(playerStore).use(router).component("font-awesome-icon", FontAwesomeIcon).mount('#app')
