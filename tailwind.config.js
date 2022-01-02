c530671890fea0293d41a014b41e4fa2361c451e
Rylee George <ryleegeorge1@gmail.com>
2022-01-02 20:37:58 +1100

    Add marquee

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,22 @@ module.exports = {
       yellow: colors.amber,
       purple: colors.purple,
       pink: colors.pink,
+    },
+    extend: {
+      animation: {
+        marquee: 'marquee 15s linear infinite',
+        marquee2: 'marquee2 15s linear infinite',
+      },
+      keyframes: {
+        marquee: {
+          '0%': { transform: 'translateX(0%)' },
+          '100%': { transform: 'translateX(-100%)' },
+        },
+        marquee2: {
+          '0%': { transform: 'translateX(100%)' },
+          '100%': { transform: 'translateX(0%)' },
+        },
+      },
     }
   },
   plugins: [require('@vueform/slider/tailwind'), require('daisyui'),  require('tailwind-scrollbar')],
