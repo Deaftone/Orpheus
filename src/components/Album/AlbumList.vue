1c3f2c13b7cb34e27151505285b04e226b30d396
Rylee George <ryleegeorge1@gmail.com>
2022-04-06 04:06:58 +1000

    Update album list and infinte scroll tester

--- src/components/Album/AlbumList.vue
+++ src/components/Album/AlbumList.vue
@@ -1,37 +1,48 @@
 <template>  
   <div class="m-5">
-    <div class="grid grid-cols-1 gap-5 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 2xl:grid-cols-8">
+    <div
+      id="list"
+      class="grid gap-5 resultgrid-cols-1 result sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 2xl:grid-cols-8"
+    >
       <div 
         v-for="album in filterAlbums"
         :key="album.id"
-        class="shadow-xl card image-full"
+        class="shadow-xl w-94 h-94 card image-full "
         @click=" $router.push({path: `/AlbumDetails/${album.id}`})
         "
       >
         <figure>
-          <img
-            class="object-contain"
-            :src="album.cover"
-          >
+          <img :src="album.cover">
         </figure>
         <div class="justify-center card-body">
           <div class="text-center transition-opacity" />
-          <h3 class="text-center">
-            <a>{{ album.name }}</a>
-          </h3> 
+          <a class="text-center text-clip overflow-hidden ... w-24 h-24">
+            {{ album.name }}
+          </a> 
         </div>
-      </div> 
+      </div>
+
+      <div
+        ref="sticky"
+        class=""
+        style="height:0.1px"
+      />
     </div>
   </div>
 </template>
 
 <script>
 import axios from '../../utils/apiAxios'
+import "v3-infinite-loading/lib/style.css";
 export default {
   name: 'AlbumList',
+    components: {
+  },
   data() {
     return {
       albums: [],
+      offset: 0,
+      firstLoad: true,
     }
   },
   computed:{
@@ -56,16 +67,51 @@ export default {
       console.log(query)
     }
   },
-  async mounted(){
-    const data = (await axios.get('/getAlbumList', {params: {
+  created() {
+    this.observer = new IntersectionObserver(
+      this.onElementObserved, 
+      {
+        root: null,
+        threshold: 0.9,
+      }
+    )
+  },
+   async mounted(){
+         this.observer.observe(this.$refs.sticky)
+
+/*     const data = (await axios.get('/getAlbumList', {params: {
       type: 'newest',
-      size: 500
+      size: 50
     }})).data
     const albums = data['subsonic-response']['albumList']['album']
     for(const album of albums){
       this.albums.push({name: album.title, id: album.id, cover: `https://navi.raspi.local/rest/getCoverArt?u=${axios.defaults.params.u}&s=${axios.defaults.params.s}&t=${axios.defaults.params.t}&f=json&c=Orpheus&v=1.8.0&id=${album.id}&size=300`})
     }
+    this.offset += 50 */
+  }, 
+  methods:{
+    async getAlbums(){
+
+      console.log('Got album call')
+      console.log(this.offset)
+      const data = (await axios.get('/getAlbumList', {params: {
+        type: 'newest',
+        size: 56,
+        offset: this.offset
+      }})).data
+      const albums = data['subsonic-response']['albumList']['album']
+      for(const album of albums){
+        this.albums.push({name: album.title, id: album.id, cover: `https://navi.raspi.local/rest/getCoverArt?u=${axios.defaults.params.u}&s=${axios.defaults.params.s}&t=${axios.defaults.params.t}&f=json&c=Orpheus&v=1.8.0&id=${album.id}&size=300`})
+      }
+      this.offset += 56
+    },
+    onElementObserved(e) {
+      e.forEach(({ target, isIntersecting}) => {
+        this.getAlbums()
+      })
+    },
   },
+  
 }
 </script>
 
