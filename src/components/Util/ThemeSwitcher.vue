30b506df47d08c38f80b66a48246901231bcd024
Rylee George <ryleegeorge1@gmail.com>
2022-04-06 04:05:40 +1000

    Add new themes

--- src/components/Util/ThemeSwitcher.vue
+++ src/components/Util/ThemeSwitcher.vue
@@ -49,27 +49,7 @@ export default {
   data() {
     return {
       themes: [
-        'light',
-        'dark',
-        'cupcake',
-        'bumblebee',
-        'emerald',
-        'corporate',
-        'synthwave',
-        'retro',
-        'cyberpunk',
-        'valentine',
-        'halloween',
-        'garden',
-        'forest',
-        'aqua',
-        'lofi',
-        'pastel',
-        'fantasy',
-        'wireframe',
-        'black',
-        'luxury',
-        'dracula',
+      "light", "dark", "cupcake", "bumblebee", "emerald", "corporate", "synthwave", "retro", "cyberpunk", "valentine", "halloween", "garden", "forest", "aqua", "lofi", "pastel", "fantasy", "wireframe", "black", "luxury", "dracula", "cmyk", "autumn", "business", "acid", "lemonade", "night", "coffee", "winter"
       ]
     }
   },
