220c2bd10376ae01b59189ef00aa0c0bc301e4a4
Rylee George <ryleegeorge1@gmail.com>
2022-04-20 18:20:27 +1000

    Change home screen

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,31 +1,67 @@
-<template> 
-  <div class="relative flex overflow-x-hidden">
-    <div class="py-12 animate-marquee whitespace-nowrap">
-      <span class="mx-4 text-4xl">Marquee Item 1</span>
-      <span class="mx-4 text-4xl">Marquee Item 2</span>
-      <span class="mx-4 text-4xl">Marquee Item 3</span>
-      <span class="mx-4 text-4xl">Marquee Item 4</span>
-      <span class="mx-4 text-4xl">Marquee Item 5</span>
+<script>
+import AlbumList from './Album/AlbumList.vue'
+import ArtistList from './Artist/ArtistList.vue'
+export default {
+  name: 'Home',
+  components: { AlbumList, ArtistList },
+  props: {
+    msg: String,
+  },
+}
+</script>
+<template>
+  <div class="grid justify-center">
+    <div class="p-10 text-xl shadow-xl stats">
+      <div class="stat">
+        <div class=" stat-title">
+          Albums
+        </div>
+        <div class="stat-value">
+          400
+        </div>
+      </div>
+      <div class="stat">
+        <div class="stat-title">
+          Songs
+        </div>
+        <div class="stat-value">
+          51,000
+        </div>
+      </div>
+      <div class="stat">
+        <div class="stat-figure text-secondary">
+          <font-awesome-icon
+            icon="microphone-alt"
+            class=""
+            color="gray"
+          />
+        </div>
+        <div class="stat-title">
+          Artists
+        </div>
+        <div class="stat-value" />
+        <div class="stat-value">
+          1,000
+        </div>
+      </div>
     </div>
-
-    <div class="absolute top-0 py-12 animate-marquee2 whitespace-nowrap">
-      <span class="mx-4 text-4xl">Marquee Item 1</span>
-      <span class="mx-4 text-4xl">Marquee Item 2</span>
-      <span class="mx-4 text-4xl">Marquee Item 3</span>
-      <span class="mx-4 text-4xl">Marquee Item 4</span>
-      <span class="mx-4 text-4xl">Marquee Item 5</span>
-    </div>
+  </div>
+  <div class="divider" />
+  <div>
+    <h2 class="pl-10">
+      New Albums
+    </h2>
+    <AlbumList :size="7" />
+  </div>
+  <div class="divider" />
+  <div>
+    <h2 class="pl-10">
+      New Arists
+    </h2>
+    <ArtistList :size="11" />
   </div>
 </template>
 
-<script>
-export default {
-  name: 'Home',
-  props: {
-    msg: String
-  }
-}
-</script>
 <style src="@vueform/slider/themes/default.css"></style>
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
