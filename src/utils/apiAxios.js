889c6bd22e489ddf5705760306f46be0dcfb6d5f
Rylee George <ryleegeorge1@gmail.com>
2022-04-20 18:44:22 +1000

    Update to class

--- src/utils/apiAxios.js
+++ src/utils/apiAxios.js
@@ -1,21 +1,24 @@
 import axios from 'axios'
-const apiAxios = axios.create({
-  baseURL: 'https://navi.raspi.local/rest/',
-  timeout: 1000,
-  headers: { 'Content-Type': 'application/json' },
-  params: { 
-    u: '***REMOVED***',
-    t: '***REMOVED***',
-    s: '558dbf',
-    f: 'json',
-    c: 'Orpheus',
-    v: '1.8.0'
+class ApolloConnector {
+  constructor() {
+    this.axios = axios.create({
+      baseURL: 'https://navi.raspi.local/rest/',
+      timeout: 1000,
+      headers: { 'Content-Type': 'application/json' },
+      params: {
+        u: '***REMOVED***',
+        t: '***REMOVED***',
+        s: '558dbf',
+        f: 'json',
+        c: 'Orpheus',
+        v: '1.8.0',
+      },
+    })
   }
-})
 
-const getCover = function (id){
-  return `https://navi.raspi.local/rest/getCoverArt?u=${apiAxios.defaults.params.u}&s=${apiAxios.defaults.params.s}&t=${apiAxios.defaults.params.t}&f=json&c=Orpheus&v=1.8.0&id=${id}`
+  getCover(id) {
+    return `https://navi.raspi.local/rest/getCoverArt?u=${this.axios.defaults.params.u}&s=${this.axios.defaults.params.s}&t=${this.axios.defaults.params.t}&f=json&c=Orpheus&v=1.8.0&id=${id}`
+  }
 }
 
-
-export default apiAxios
+export default ApolloConnector
