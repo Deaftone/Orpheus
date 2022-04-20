dfa937e8fb7febf238be4a7df3fb93fa0236e1b5
Rylee George <ryleegeorge1@gmail.com>
2022-04-20 18:19:50 +1000

    Format

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,27 @@
 import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 const { resolve } = require('path')
-//import path from 'path'
-//import ViewLauncherVuePlugin from '@view-launcher/rollup-plugin-vue'
+// import path from 'path'
+// import ViewLauncherVuePlugin from '@view-launcher/rollup-plugin-vue'
 export default defineConfig(({ mode }) => {
   // load the .env file
   // you may want to create one like `.env.development.local`
-  //const env = loadEnv(mode, __dirname)
+  // const env = loadEnv(mode, __dirname)
 
   return {
     build: {
       rollupOptions: {
         input: {
           main: resolve(__dirname, 'index.html'),
-          nested: resolve(__dirname, 'splashscreen.html')
-        }
-      }
+          nested: resolve(__dirname, 'splashscreen.html'),
+        },
+      },
     },
     server: {
       fs: {
         // Allow serving files from one level up to the project root
-        allow: ['..']
-      }
+        allow: ['..'],
+      },
     },
     // base: path.resolve(__dirname, './dist/'),
     plugins: [
@@ -31,10 +31,10 @@ export default defineConfig(({ mode }) => {
         editor: env.VITE_EDITOR || 'vscode',
       }), */
       vue(),
-      
+
     ],
     define: {
-      'process.env': {}
-    }
+      'process.env': {},
+    },
   }
 })
