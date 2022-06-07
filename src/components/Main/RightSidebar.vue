5481f2a0f53ae347238d21faadb12403e48fb305
Rylee George <ryleegeorge1@gmail.com>
2022-06-07 21:54:03 +1000

    Move to comp api

--- src/components/Main/RightSidebar.vue
+++ src/components/Main/RightSidebar.vue
@@ -1,6 +1,6 @@
 <script >
 import draggable from 'vuedraggable'
-import { computed, ref, watch } from 'vue'
+import { computed, reactive, ref, watch } from 'vue'
 import { usePlayerStore } from '../../stores/player'
 export default {
   components: {
@@ -8,102 +8,63 @@ export default {
   },
   setup() {
     const store = usePlayerStore()
-
     const playingQueue = computed(() => store.queue)
-    const nowPlaying = ref(store.nowPlaying)
+    const nowPlaying = computed(() => store.nowPlaying)
 
-    watch(nowPlaying, (currentValue, oldPlaying) => {
+    watch(nowPlaying, (newValue, oldValue) => {
       // Absolute hack. We need to delay the highlighting of the now playing on the sidebar or else its no rendered when we call to highlight
       setTimeout(() => {
-        this.highLightNowPlaying(oldPlaying, newPlaying)
+        let oldP
+
+        if (oldValue)
+          oldP = document.getElementById(`${oldValue.id}_sidebar`)
+
+        const newP = document.getElementById(`${newValue.id}_sidebar`)
+
+        if (oldP) {
+          document.getElementById(`${oldValue.id}_scroller`).classList.remove('animate-marquee')
+          document.getElementById(`${oldValue.id}_scroller2`).classList.remove('animate-marquee2')
+
+          oldP.classList.remove('gradient-border', 'border-solid', 'border-4', 'border-primary')
+        }
+        if (newP) {
+          document.getElementById(`${newValue.id}_scroller`).classList.add('animate-marquee')
+          document.getElementById(`${newValue.id}_scroller2`).classList.add('animate-marquee2')
+
+          newP.classList.add('gradient-border', 'border-solid', 'border-4', 'border-primary')
+        }
       }, 10)
     })
-
-    console.log(playingQueue.value)
-
-    function highLightNowPlaying(oldPlaying, newPlaying) {
-      console.log(`High${newPlaying.title}`)
-      let oldP
-
-      if (oldPlaying)
-        oldP = document.getElementById(`${oldPlaying.id}_sidebar`)
-
-      const newP = document.getElementById(`${newPlaying.id}_sidebar`)
-
-      if (oldP) {
-        document.getElementById(`${oldPlaying.id}_scroller`).classList.remove('animate-marquee')
-        document.getElementById(`${oldPlaying.id}_scroller2`).classList.remove('animate-marquee2')
-
-        oldP.classList.remove('gradient-border', 'border-solid', 'border-4', 'border-primary')
-      }
-      if (newP) {
-        document.getElementById(`${newPlaying.id}_scroller`).classList.add('animate-marquee')
-        document.getElementById(`${newPlaying.id}_scroller2`).classList.add('animate-marquee2')
-
-        newP.classList.add('gradient-border', 'border-solid', 'border-4', 'border-primary')
-      }
-    }
-
-    function setQueue(value) {
-      store.commit('setQueue', value)
-    }
-
     return {
       playingQueue,
       nowPlaying,
     }
   },
-  /*   computed: {
-    playingQueue() {
-      return store.state.queue
-    },
-    nowPlaying() {
-      return store.state.nowPlaying
-    },
-    queue: {
-      get() {
-        return store.state.queue
-      },
-      set(value) {
-        store.commit('setQueue', value)
-      },
-    },
-  }, */
-/*   watch: {
-    playingQueue(newQueue) {
-      this.queue = newQueue
-    },
-    nowPlaying(newPlaying, oldPlaying) {
-      // Absolute hack. We need to delay the highlighting of the now playing on the sidebar or else its no rendered when we call to highlight
-      setTimeout(() => {
-        this.highLightNowPlaying(oldPlaying, newPlaying)
-      }, 10)
-    },
-  }, */
 }
 </script>
 
 <template>
   <draggable
     v-model="playingQueue"
+    :list="playingQueue"
     tag="ul"
     class="overflow-y-scroll scrollbar-thin scrollbar-track-gray-800 scrollbar-thumb-gray-500 hover:scrollbar-thumb-green-700"
     item-key="id"
   >
-    <template #item="{element}">
+    <template #item="{ element }">
       <li
-        :id="element.id + '_sidebar'"
+        :id="`${element.id}_sidebar`"
         class="items-center justify-between p-3 m-3 text-sm transition duration-300 ease-in-out delay-150 rounded-lg shadow-xl cursor-move bg-base-100 hover:scale-105"
       >
         <div class="relative flex overflow-x-hidden">
           <div
-            :id="element.id + '_scroller'"
+            :id="`${element.id}_scroller`"
             class="whitespace-nowrap"
           >
             <span class="text-primary">&nbsp;{{ element.artist }}</span><span class=""> - {{ element.title }}</span>
           </div>
           <div
-            :id="element.id + '_scroller2'"
+            :id="`${element.id}_scroller2`"
             class="absolute top-0 whitespace-nowrap"
           >
             <span class="text-primary">&nbsp;{{ element.artist }}</span><span class=""> - {{ element.title }}</span>
