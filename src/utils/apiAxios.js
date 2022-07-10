b5f5aa78c21248e951924832660899a5a049e628
Rylee George <13013625+112RG@users.noreply.github.com>
2022-07-11 03:57:57 +1000

    Cleanup axios file

--- src/utils/apiAxios.js
+++ src/utils/apiAxios.js
@@ -1,45 +1,49 @@
 import axios from 'axios'
+import axiosTauriAdapter from 'axios-tauri-adapter'
 const userName = import.meta.env.VITE_USERNAME
 const token = import.meta.env.VITE_TOKEN
 const s = import.meta.env.VITE_S
+
 class ApolloConnector {
   constructor() {
     console.log(userName)
     this.axios = axios.create({
-      baseURL: 'https://navi.raspi.local/rest/',
+      adapter: window.__TAURI__ ? axiosTauriAdapter : null,
+      baseURL: 'https://apollo.raspi.local',
       timeout: 5000,
       headers: { 'Content-Type': 'application/json' },
-      params: {
-        u: userName,
-        t: token,
-        s,
-        f: 'json',
-        c: 'Orpheus',
-        v: '1.8.0',
-      },
     })
   }
 
   async getArtists() {
-    return (await this.axios.get('/getArtists')).data
+    return (await this.axios.get('/artists')).data
   }
 
   async getArtist(artistId) {
-    return (await this.axios.get('/getArtist', {
-      params: {
-        id: artistId,
-      },
-    })).data
+    return (await this.axios.get(`/artists/${artistId}`)).data
   }
 
   async getAlbums(size, offset) {
-    return (await this.axios.get('/getAlbumList', {
+    /*  return (await this.axios.get('/getAlbumList', {
       params: {
         type: 'newest',
         size,
         offset,
       },
+    })).data */
+    return await this.axios.get('/getAlbumList')
+  }
+
+  async getArtistAlbum(albumId) {
+    return (await this.axios.get('/getArtistAlbum', {
+      params: {
+        id: albumId,
+      },
     })).data
+  }
+
+  stream(id) {
+    return `${this.axios.defaults.baseURL}/stream/${id}`
   }
 
   async getArtistAlbums(artistId) {
@@ -51,7 +55,7 @@ class ApolloConnector {
   }
 
   getCover(id) {
-    return `https://navi.raspi.local/rest/getCoverArt?u=${this.axios.defaults.params.u}&s=${this.axios.defaults.params.s}&t=${this.axios.defaults.params.t}&f=json&c=Orpheus&v=1.8.0&id=${id}`
+    // return `https://navi.raspi.local/rest/getCoverArt?u=${this.axios.defaults.params.u}&s=${this.axios.defaults.params.s}&t=${this.axios.defaults.params.t}&f=json&c=Orpheus&v=1.8.0&id=${id}`
   }
 }
 
