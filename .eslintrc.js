5a0d533940e616124cbe1af6e1e15f5902a5fa47
Rylee George <13013625+112RG@users.noreply.github.com>
2022-07-13 04:45:35 +1000

    Remove antfu

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,14 +4,16 @@ module.exports = {
     node: true,
   },
   extends: [
-    'plugin:vue/vue3-recommended',
-    '@antfu',
+    'plugin:vue/vue3-strongly-recommended',
   ],
   parserOptions: {
     ecmaVersion: 2021,
   },
   rules: {
     'no-var': 'error',
+    "vue/component-api-style": ["error",
+    ["script-setup", "composition"] // "script-setup", "composition", "composition-vue2", or "options"
+  ]
   },
   globals: {
     defineProps: 'readonly',
