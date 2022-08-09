9b8dcd3f0512914798abf1ed1e3e0dc1c00f530c
Rylee George <ryleegeorge1@gmail.com>
2022-08-09 13:00:53 +1000

    Cleanup API

--- src/services/deaftoneApi.js
+++ src/services/deaftoneApi.js
@@ -1,12 +1,9 @@
 import axios from 'axios'
 import axiosTauriAdapter from 'axios-tauri-adapter'
 const userName = import.meta.env.VITE_USERNAME
-const token = import.meta.env.VITE_TOKEN
-const s = import.meta.env.VITE_S
 
 class DeaftoneConnector {
   constructor () {
-    console.log(userName)
     this.axios = axios.create({
       adapter: window.__TAURI__ ? axiosTauriAdapter : null,
       baseURL: 'https://apollo.raspi.local',
@@ -24,13 +21,6 @@ class DeaftoneConnector {
   }
 
   async getAlbums (size, offset) {
-    /*  return (await this.axios.get('/getAlbumList', {
-      params: {
-        type: 'newest',
-        size,
-        offset,
-      },
-    })).data */
     return await this.axios.get('/albums', {
       params: {
         size, offset
@@ -46,10 +36,6 @@ class DeaftoneConnector {
     })).data
   }
 
-  stream (id) {
-    return `${this.axios.defaults.baseURL}/stream/${id}`
-  }
-
   async getArtistAlbums (artistId) {
     return (await this.axios.get('/getAlbum', {
       params: {
@@ -58,9 +44,12 @@ class DeaftoneConnector {
     })).data
   }
 
+  stream (id) {
+    return `${this.axios.defaults.baseURL}/stream/${id}`
+  }
+
   getCover (id) {
-    return `https://apollo.raspi.local/albums/${id}/cover`
-    // return `https://navi.raspi.local/rest/getCoverArt?u=${this.axios.defaults.params.u}&s=${this.axios.defaults.params.s}&t=${this.axios.defaults.params.t}&f=json&c=Orpheus&v=1.8.0&id=${id}`
+    return `${this.axios.defaults.baseURL}/albums/${id}/cover`
   }
 }
 
