d4e5d63ef9b14ddf72d4075aa6f7ab04010e50b3
Rylee George <ryleegeorge1@gmail.com>
2022-08-30 21:29:10 +1000

    Add deaftone cache

--- src/services/deaftoneApi.js
+++ src/services/deaftoneApi.js
@@ -1,14 +1,54 @@
 import axios from 'axios'
 import axiosTauriAdapter from 'axios-tauri-adapter'
-
+import {
+  setupCache,
+  defaultKeyGenerator,
+  defaultHeaderInterpreter,
+  buildWebStorage
+} from 'axios-cache-interceptor'
 class DeaftoneConnector {
   constructor () {
-    this.axios = axios.create({
-      adapter: window.__TAURI__ ? axiosTauriAdapter : null,
+    /*     this.axios = axios.create({
+      adapter: window.__TAURI__ ? axiosTauriAdapter : null, this.cache],
       baseURL: 'https://apollo.raspi.local',
       timeout: 5000,
       headers: { 'Content-Type': 'application/json' }
-    })
+    }) */
+    this.cacheStorage = buildWebStorage(localStorage, 'axios-cache:')
+    this.axios = setupCache(
+      // axios instance
+      axios.create({
+        adapter: window.__TAURI__ ? axiosTauriAdapter : null,
+        baseURL: 'https://apollo.raspi.local',
+        timeout: 5000,
+        headers: { 'Content-Type': 'application/json' }
+      }),
+
+      // All options with their default values
+      {
+        // The storage to save the cache data. There are more available by default.
+        //
+        // https://axios-cache-interceptor.js.org/#/pages/storages
+        storage: this.cacheStorage,
+
+        // The mechanism to generate a unique key for each request.
+        //
+        // https://axios-cache-interceptor.js.org/#/pages/request-id
+        generateKey: defaultKeyGenerator,
+
+        // The mechanism to interpret headers (when cache.interpretHeader is true).
+        //
+        // https://axios-cache-interceptor.js.org/#/pages/global-configuration?id=headerinterpreter
+        headerInterpreter: defaultHeaderInterpreter,
+
+        // The function that will receive debug information.
+        // NOTE: For this to work, you need to enable development mode.
+        //
+        // https://axios-cache-interceptor.js.org/#/pages/development-mode
+        // https://axios-cache-interceptor.js.org/#/pages/global-configuration?id=debug
+        debug: undefined
+      }
+    )
   }
 
   async getArtists () {
