caf1e9204306bfb78c271fa211a31f0ee4922519
Rylee George <ryleegeorge1@gmail.com>
2022-09-20 01:04:15 +1000

    Add settings page

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -8,7 +8,45 @@ export default {
 </script>
 
 <template>
-  <a>Settings</a>
+  <div class="flex flex-col flex-1 pt-10 pb-10 pl-24 pr-24 m-10 rounded-md shadow-md mr-36 ml-36 h-1/2 bg-neutral">
+    <h1 class="font-bold">
+      Server settings
+    </h1>
+    <div class="divider" />
+    <div class="flex form-control">
+      <label class="flex label">
+        <span class="label-text">Server address</span>
+      </label>
+      <label class="input-group">
+        <input
+          type="text"
+          placeholder="http://localhost:3030"
+          class="input input-bordered"
+        >
+      </label>
+    </div>
+    <div class="flex form-control">
+      <label class="flex label">
+        <span class="label-text">Server type</span>
+      </label>
+      <select class="w-full max-w-xs select select-bordered">
+        <option
+          disabled
+          selected
+        >
+          Deaftone
+        </option>
+        <option>Subsonic</option>
+        <option>Navidrome</option>
+      </select>
+    </div>
+    <div class="flex justify-center">
+      <button class="btn btn-primary">
+        Save
+      </button>
+    </div>
+    <div class="divider" />
+  </div>
 </template>
 
 <style scoped>
