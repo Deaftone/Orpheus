888cf95e99814e6f40c68fdb6a43971d0c919eca
Rylee George <ryleegeorge1@gmail.com>
2022-12-23 01:17:46 +1100

    Update to use vlazy

--- src/components/Album/AlbumCard.vue
+++ src/components/Album/AlbumCard.vue
@@ -1,13 +1,18 @@
 <script>
 import 'v3-infinite-loading/lib/style.css'
+import imgUrl from '../../assets/unknown_album.jpg'
 import { inject } from 'vue'
+import VLazyImage from 'v-lazy-image'
 export default {
   props: { name: { type: String, required: true }, id: { type: String, required: true } },
+  components: { VLazyImage },
+
   setup (props) {
     const deaftone = inject('$deaftone')
 
     return {
       deaftone,
+      imgUrl,
       props
     }
   }
@@ -15,11 +20,11 @@ export default {
 </script>
 <template>
   <div class="overflow-hidden rounded-lg shadow-xl">
-    <img
+    <VLazyImage
       :src="deaftone.getCover(props.id)"
+      :src-placeholder="imgUrl"
       style="width: auto; height: auto"
-    >
-
+    />
     <div class="justify-center pt-5 pb-5 card-body">
       <p class="text-center truncate">
         {{ props.name }}
