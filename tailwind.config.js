8ed397bda3e4e8b39f23422df95474132d368e36
Rylee George <ryleegeorge1@gmail.com>
2023-01-10 19:04:04 +1100

    Update view breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -41,5 +41,13 @@ module.exports = {
     utils: true,
     logs: true,
     rtl: false
+  },
+  screens: {
+    xs: { max: '575px' }, // Mobile (iPhone 3 - iPhone XS Max).
+    sm: { min: '576px', max: '897px' }, // Mobile (matches max: iPhone 11 Pro Max landscape @ 896px).
+    md: { min: '898px', max: '1199px' }, // Tablet (matches max: iPad Pro @ 1112px).
+    lg: { min: '1200px' }, // Desktop smallest.
+    xl: { min: '1159px' }, // Desktop wide.
+    xxl: { min: '1359px' } // Desktop widescreen.
   }
 }
